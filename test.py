adb6a6c7ca38fda5cceb4d90ad1f87489ac2f029
Kris Jeon <poucotm26@gmail.com>
2020-08-16 23:08:58 +0900

    asdfdsf

--- test.py
+++ test.py
@@ -16,7 +16,11 @@ def plugin_loaded():
             print ('install testing')
         elif events.post_upgrade('testing'):
             print ('post_upgrade testing')
-    print ('plugin_loaded')
+
+    get_log_extension()
+    lhs = get_prefs()
+    lhs.clear_on_change('testing-prefs')
+    lhs.add_on_change('testing-prefs', get_log_extension)
 
 
 def plugin_unloaded():
@@ -26,3 +30,26 @@ def plugin_unloaded():
         elif events.pre_upgrade('testing'):
             print ('pre_upgrade testing')
     print ('plugin_unloaded')
+
+
+def get_prefs():
+    return sublime.load_settings('testing.sublime-settings')
+
+def get_log_extension():
+    global EXT_ALL
+    global EXT_DIC
+    global OUT_DIC
+    EXT_ALL = []
+    EXT_DIC = {}
+    OUT_DIC = {}
+    lol = get_prefs().get('log_list')
+    lgl = list(lol.keys())
+    for ltype in lgl:
+        extl = lol.get(ltype).get('extension')
+        for ext in extl:
+            EXT_ALL.append(ext)
+            EXT_DIC[ext] = ltype
+        outl = lol.get(ltype).get('output.panel')
+        for out in outl:
+            OUT_DIC[out] = ltype
+    return
